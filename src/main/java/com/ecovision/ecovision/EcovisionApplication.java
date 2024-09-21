f545bae2f334b2e98b17302e34844c977eebfc70
kymin <park48664866@gmail.com>
2024-09-21 13:42:28 +0900

    initial commit

---
+++ src/main/java/com/ecovision/ecovision/EcovisionApplication.java
@@ -0,0 +1,13 @@
+package com.ecovision.ecovision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcovisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcovisionApplication.class, args);
+	}
+
+}
