f7770d3fba29fc4368962c8bcbb0d551800520fb
hwangtaehyeon123 <hddhdd010515@naver.com>
2024-10-04 22:24:00 +0900

    feat:repository 수정

--- src/main/java/com/ecovision/ecovision/repository/PloggingRepository.java
+++ src/main/java/com/ecovision/ecovision/repository/PloggingRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+//ploggingRepository
 public interface PloggingRepository extends JpaRepository<Plogging, Long>{
 
 }
