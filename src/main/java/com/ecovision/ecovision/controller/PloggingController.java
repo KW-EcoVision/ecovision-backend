9103650783dd4f6e56d2b894f43a4c673ac9f453
hwangtaehyeon123 <hddhdd010515@naver.com>
2024-10-04 22:22:19 +0900

    feat:controller수정

--- src/main/java/com/ecovision/ecovision/controller/PloggingController.java
+++ src/main/java/com/ecovision/ecovision/controller/PloggingController.java
@@ -1,9 +1,6 @@
 package com.ecovision.ecovision.controller;
 
-import com.ecovision.ecovision.dto.PloggingRequestDto;
-import com.ecovision.ecovision.dto.PloggingResponseDto;
-import com.ecovision.ecovision.dto.PloggingViewResponseDto;
-import com.ecovision.ecovision.dto.TotalViewResponseDto;
+import com.ecovision.ecovision.dto.*;
 import com.ecovision.ecovision.service.PloggingService;
 import com.ecovision.ecovision.service.TotalViewService;
 import org.springframework.http.HttpStatus;
@@ -15,47 +12,68 @@ import java.util.List;
 @RequestMapping("/plogging")
 public class PloggingController {
 
+
+    //의존성 주입
     private final PloggingService ploggingService;
     private final TotalViewService totalViewService;
     public PloggingController(PloggingService ploggingService, TotalViewService totalViewService) {
         this.ploggingService = ploggingService;
         this.totalViewService = totalViewService;
     }
+
     //기록 생성
     @PostMapping("/record")
     public ResponseEntity<?> record(@RequestBody PloggingRequestDto requestDto) {
-        PloggingResponseDto responseDto = ploggingService.createPlogging(requestDto);
-        return ResponseEntity.ok(responseDto);
+        try {
+        PloggingResponseDto response = ploggingService.createPlogging(requestDto);
+        totalViewService.createTotalPlogging(requestDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response); //201
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage()); //404
+        }
     }
+
     //회차별 기록 조회
     @GetMapping("/view")
     public ResponseEntity<?> view(@RequestParam(name = "id") Long id) {
         try {
             PloggingResponseDto response = ploggingService.ploggingViewById(id);
-            return ResponseEntity.ok(response);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
         } catch (NullPointerException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
-    //username -> plgging view list
-    @GetMapping("/listview")
-    public ResponseEntity<?> listView(@RequestParam(name = "username") String username) {
-        try {
-            List<PloggingViewResponseDto> response = ploggingService.ploggingListViewByUsername(username);
-            return ResponseEntity.ok(response);
-        } catch (NullPointerException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
-    }
-    //username -> plogging total view ( 모든 회차 합 )
-    @GetMapping("/totalview")
-    public ResponseEntity<?> totalView(@RequestParam(name = "username") String username) {
-        try {
-            TotalViewResponseDto response = totalViewService.ploggingTotalViewByUsername(username);
-            return ResponseEntity.ok(response);
-        } catch (NullPointerException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
 
+    //유저 기록 리스트 조회
+    @GetMapping("/listview")
+    public ResponseEntity<?> listView() {
+        try {
+            List<PloggingViewResponseDto> response = ploggingService.ploggingListViewByUsername();
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    //유저 모든 기록 합 조회
+    @GetMapping("/totalview")
+    public ResponseEntity<?> totalView() {
+        try {
+            TotalViewResponseDto response = totalViewService.ploggingTotalViewByUsername();
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    //유저 모든 기록 합 + 기록 리스트 조회
+    @GetMapping("/totalandlistview")
+    public ResponseEntity<?> totalAndListView() {
+        try {
+            TotalViewAndPloggingListDto response = totalViewService.totalViewAndPloggingListByUsername();
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
--- src/main/java/com/ecovision/ecovision/controller/UserController.java
+++ src/main/java/com/ecovision/ecovision/controller/UserController.java
@@ -1,18 +1,15 @@
 package com.ecovision.ecovision.controller;
 
 import com.ecovision.ecovision.dto.JoinDto;
-import com.ecovision.ecovision.dto.UserRequestDto;
+import com.ecovision.ecovision.dto.NameUpdateRequestDto;
+import com.ecovision.ecovision.dto.PasswordUpdateRequestDto;
 import com.ecovision.ecovision.dto.UserResponseDto;
 import com.ecovision.ecovision.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.Iterator;
 
 @ResponseBody
 @RestController
@@ -21,56 +18,64 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     //회원가입
     @PostMapping("/join")
-    public String JoinProcess(JoinDto joinDto) {
-
-        userService.joinProcess(joinDto);
-
-        return "회원가입에 성공하셨습니다.";
-    }
-
-    //역할
-    @GetMapping("/admin")
-    public String adminP() {
-
-        return "Admin";
-    }
-
-    @GetMapping("/")
-    public String mainP() {
-
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
-        GrantedAuthority auth = iter.next();
-        String role = auth.getAuthority();
-
-        return "Main Controller : " + username + role;
-
-    }
-
-    //유저 정보 수정
-    @PutMapping("/update")
-    public ResponseEntity<?> userUpdate(@RequestParam(name = "username") String username, String password, @RequestBody UserRequestDto userRequestDto) {
+    public ResponseEntity<?> JoinProcess(JoinDto joinDto) {
         try {
-            UserResponseDto response = userService.updateUserDetails(username, password, userRequestDto);
-            return ResponseEntity.ok(response);
+            String response = userService.joinProcess(joinDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    //유저 정보 조회
+    @GetMapping("/userview")
+    public ResponseEntity<?> getUserView() {
+        try {
+            UserResponseDto response = userService.UserView();
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (NullPointerException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    //유저 이름 수정
+    @PutMapping("/updatename")
+    public ResponseEntity<?> userNameUpdate(@RequestBody NameUpdateRequestDto nameUpdateRequestDto) {
+        try {
+            UserResponseDto response = userService.NameUpdateDetails(nameUpdateRequestDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (NullPointerException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    //유저 패스워드 수정
+    @PutMapping("/updatepassword")
+    public ResponseEntity<?> userPasswordUpdate(@RequestBody PasswordUpdateRequestDto passwordUpdateRequestDto) {
+        try {
+            UserResponseDto response = userService.PasswordUpdateDetails(passwordUpdateRequestDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
         } catch (NullPointerException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 
     //유저 탈퇴
-    @PutMapping("/delete")
-    public ResponseEntity<String> userDelete(@RequestParam(name = "username") String username, String password) {
-        userService.userDelete(username, password);
-        return ResponseEntity.ok("Deleted");
+    @Transactional
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> userDelete() {
+        try {
+            userService.userDelete();
+            return ResponseEntity.ok("Deleted");
+        } catch (NullPointerException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
 
-
 }
+
+
