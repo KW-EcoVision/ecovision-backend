c8df75b97e921264ae98ed5f84502eec2b38998e
hwangtaehyeon123 <hddhdd010515@naver.com>
2024-10-08 22:36:00 +0900

    feat : LoginFilter -> login 헤더 변경

--- src/main/java/com/ecovision/ecovision/jwt/LoginFilter.java
+++ src/main/java/com/ecovision/ecovision/jwt/LoginFilter.java
@@ -28,17 +28,17 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        // 'application/x-www-form-urlencoded' 형식으로 username, password 추출
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
 
-        //클라이언트 요청에서 username, password 추출
-        String username = obtainUsername(request);
-        String password = obtainPassword(request);
-
-        //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
+        // 스프링 시큐리티에서 username과 password를 검증하기 위해 token에 담아야 함
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
 
-        //token에 담은 검증을 위한 AuthenticationManager로 전달
+        // token에 담은 검증을 위한 AuthenticationManager로 전달
         return authenticationManager.authenticate(authToken);
     }
+
 
     //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
     @Override
