549a0e8ae1856d3c914f106107d72f8e1d8d9f95
hwangtaehyeon123 <hddhdd010515@naver.com>
2024-10-09 15:28:58 +0900

    test

---
+++ src/main/java/com/ecovision/ecovision/dto/NameUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package com.ecovision.ecovision.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+//name update request dto
+public class NameUpdateRequestDto {
+
+    private String name;
+
+}
