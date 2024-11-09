3a23c0056097516d61fd8c323ede177b28297c00
hwangtaehyeon123 <hddhdd010515@naver.com>
2024-11-09 21:42:37 +0900

    fix: level 추가

--- src/main/java/com/ecovision/ecovision/service/TotalViewService.java
+++ src/main/java/com/ecovision/ecovision/service/TotalViewService.java
@@ -6,6 +6,7 @@ import com.ecovision.ecovision.entity.TotalView;
 import com.ecovision.ecovision.entity.User;
 import com.ecovision.ecovision.repository.TotalViewRepository;
 import com.ecovision.ecovision.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,18 @@ import java.util.List;
 
 
 @Service
+@Transactional
 public class TotalViewService {
     private final UserRepository userRepository;
     private final TotalViewRepository totalViewRepository;
+
     public TotalViewService(UserRepository userRepository, TotalViewRepository totalViewRepository) {
         this.userRepository = userRepository;
         this.totalViewRepository = totalViewRepository;
     }
 
     //기록 생성 controller -> 기록 생성 할 때 마다 (total view +=) 저장
-    public TotalViewResponseDto createTotalPlogging(PloggingRequestDto requestDto){
+    public TotalViewResponseDto createTotalPlogging(PloggingRequestDto requestDto) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userRepository.findByUsername(username);
         if (user == null) {
@@ -74,7 +77,7 @@ public class TotalViewService {
 
     }
 
-    //total view(list) + total view (sum)
+    //total view(list) + total view (sum) + level
     public TotalViewAndPloggingListDto totalViewAndPloggingListByUsername() {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userRepository.findByUsername(username);
@@ -84,7 +87,20 @@ public class TotalViewService {
 
         TotalView totalView = totalViewRepository.findByUser(user);
         if (totalView == null) {
-            throw new NullPointerException("기록이 존재하지 않습니다.");
+            totalView = new TotalView();
+            totalView.setLevel(1);
+        }
+
+        int totalScore = totalView.getTotalDistance() / 1000 + totalView.getTotalTime() / 60 + totalView.getTotalCount();
+
+        if (totalScore >= 1000) {
+            totalView.setLevel(4);
+        } else if (totalScore >= 500) {
+            totalView.setLevel(3);
+        } else if (totalScore >= 200) {
+            totalView.setLevel(2);
+        } else {
+            totalView.setLevel(1);
         }
 
         List<Plogging> ploggingList = user.getPloggingEntities();
@@ -106,7 +122,10 @@ public class TotalViewService {
                 totalView.getTotalDistance(),
                 totalView.getTotalTime(),
                 totalView.getTotalCount(),
+                totalView.getLevel(),
                 ploggingResponseList
         );
     }
-};
+
+}
+
